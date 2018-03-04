374eb9b2839473437e989a0c20f4b02138b566b0
sdthompson <36945192+sdthompson@users.noreply.github.com>
2018-03-04 09:52:40 -0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'mysql.hostinger.com';
+$user = 'u887495827_sthom';
+$pass = '3576$gM76';
+$db = 'u887495827_stdb';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
